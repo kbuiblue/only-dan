6ad300f6da2e4b113a5f812352fb8b7faf16a637
Khanh Bui <89316304+kbuiblue@users.noreply.github.com>
2023-04-25 09:37:34 +0700

    Tables and dataset for OnlyDan database

---
+++ product_info.sql
@@ -0,0 +1,706 @@
+INSERT INTO public.product_info 
+(inventory_status, stock_price, product_brand, product_name, product_condition, product_type, product_year, country_id, description)
+values
+(1,11000,'Gibson','Gibson EDS-1275 Walnut Vintage 1970','Good','Electric guitars',1970,'US','BODY
+
+BODY TYPE: Solid body
+BODY MATERIAL: Mahogany
+BODY FINISH: Lacquer
+FINISH TYPE: Gloss
+COLOR: Walnut
+
+
+NECK
+
+NECK MATERIAL: Mahogany
+NECK SHAPE: Medium “C”
+RADIUS: 12” Radius
+1st NECK THICKNESS: .840”
+12st NECK THICKNESS: .990”
+FINGERBOARD MATERIAL: Rosewood
+FINGERBOARD INLAY: Parallelogram Inlays
+NUMBER OF FRETS: 22
+SCALE LENGTH: 24.75”
+NUT WIDTH: 1.6875”
+
+
+HARDWARE
+
+BRIDGE/TAILPIECE: ABR-1 Bridge with Stopbar Tailpiece
+TUNERS: Schaller Tuners
+
+
+ELECTRONICS
+
+NECK PICKUP: Gibson “Pattern Number” Humbucker
+BRIDGE PICKUP: Gibson “Pattern Number” Humbucker
+SWITCHING:
+-3-way toggle switch in the middle of the body for pickup selection
+-3-way toggle switch on the lower horn for 6-string/12-string neck selection
+CONTROLS: 1 Volume/Tone for each neck
+
+
+NOTES
+
+Comes with Hardshell Case.'),
+(1,9500,'Fender','Fender Custom Shop Master Built Series 1962 Stratocaster Relic by Jason Smith','Excellent','Electric guitars',2016,'US','BODY
+
+BODY TYPE: Solid body
+BODY MATERIAL: Alder
+BODY FINISH: Lacquer
+FINISH TYPE: Relic
+COLOR: Sonic Blue
+
+
+NECK
+
+NECK MATERIAL: Quartersawn Maple with Reversed Headstock
+NECK SHAPE: 60s "C"
+RADIUS: 7.25" to 9.5" Vintage Compound Radius
+1st NECK THICKNESS: .800”
+12st NECK THICKNESS: .920”
+FINGERBOARD MATERIAL: Rosewood
+FINGERBOARD INLAY: White Dots
+NUMBER OF FRETS: 22, Dunlop Jumbo 6100
+SCALE LENGTH: 25.5”
+NUT WIDTH: 1.650"
+
+
+HARDWARE
+
+BRIDGE/TAILPIECE: Original Floyd Rose
+TUNERS: Fender Vintage Style
+
+
+ELECTRONICS
+
+NECK PICKUP: Fender Custom Shop Handwound 60s Single-coil
+MIDDLE PICKUP: Fender Custom Shop Handwound 60s Single-coil
+BRIDGE PICKUP: Seymour Duncan TB-4 JB Trembucker Black
+SWITCHING: 5-way pickup switch
+CONTROLS: 1 x master volume, 2 x master tone
+
+
+NOTES
+
+Comes with Original Hardshell Case, COA'),
+(1,6000,'Fender','Fender Custom Shop Classic Player Stratocaster Masterbuilt Art Esparza','Mint','Electric guitars',2002,'US','BODY
+
+BODY TYPE: Solid body
+BODY MATERIAL: Alder
+BODY FINISH: Nitro
+FINISH TYPE: Gloss
+COLOR: Aged White
+NECK
+
+NECK MATERIAL: Birdeyes Maple
+NECK SHAPE: "C"
+RADIUS: 12” Radius
+1st NECK THICKNESS:
+12st NECK THICKNESS:
+FINGERBOARD MATERIAL: Rosewood
+FINGERBOARD INLAY: White Dots
+NUMBER OF FRETS: 22, Jumbo
+SCALE LENGTH: 25.5”
+NUT WIDTH:
+HARDWARE
+
+BRIDGE/TAILPIECE: 2-point Tremolo Bridge with Fender American Series Saddles
+TUNERS: Sperzel Locking Tuners with Roller Nut
+ELECTRONICS
+
+NECK PICKUP: Fender Noiseless Singlecoil
+MIDDLE PICKUP: Fender Noiseless Singlecoil
+BRIDGE PICKUP: Fender Noiseless Singlecoil
+SWITCHING: 5-way pickup switch
+CONTROLS: 1 x master volume, 2 x master tone. 
+NOTES
+
+Comes with Original Hardshell Case, COA, Case candy.'),
+(1,6000,'Charvel','Charvel EVH Art Series Red','Very Good','Electric guitars',2004,'US','BODY
+
+BODY TYPE: Solid body
+BODY MATERIAL: Basswood
+BODY FINISH: Poly
+COLOR: EVH Red
+
+
+NECK
+
+NECK MATERIAL: Maple
+NECK SHAPE: Medium “C”
+RADIUS: 12”
+1st NECK THICKNESS: .800"
+12st NECK THICKNESS: .920”
+FINGERBOARD MATERIAL: Maple
+FINGERBOARD INLAY: Black Dots
+NUMBER OF FRETS: 22, Jumbo
+SCALE LENGTH: 25.5”
+NUT WIDTH: 1.625”
+
+
+HARDWARE
+
+BRIDGE/TAILPIECE: Original Floyd Rose with D-Tuna
+TUNERS: Schaller Tuners
+
+
+ELECTRONICS
+
+BRIDGE PICKUP: EVH Humbucker
+CONTROLS: 1 x master volume
+
+
+NOTES
+
+Comes with Original Hardshell Case'),
+(1,5500,'Paoletti ','Paoletti Loft Series Stratospheric HSS Cream w/Reverse Headstock','Mint','Electric guitars',2019,'IT','BODY
+
+BODY TYPE: Solid body
+BODY MATERIAL: Reclaimed Chestnut
+BODY FINISH: Nitro
+FINISH TYPE: Heavy Relic
+COLOR: Cream
+
+
+NECK
+
+NECK MATERIAL: Roasted Canadian Maple
+NECK SHAPE: Medium "C"
+RADIUS: 12” Radius
+1st NECK THICKNESS:
+12st NECK THICKNESS:
+FINGERBOARD MATERIAL: Pau Ferro
+FINGERBOARD INLAY: MOP Dots
+NUMBER OF FRETS: 22
+SCALE LENGTH: 25.5”
+NUT WIDTH: 1.650”
+
+
+HARDWARE
+
+BRIDGE/TAILPIECE: Paoletti Brass
+TUNERS: Kluson Deluxe
+
+
+ELECTRONICS
+
+NECK PICKUP: Paoletti 60s Single Coil
+MIDDLE PICKUP: Paoletti 60s Single-coil
+BRIDGE PICKUP: Paoletti Rock II Humbucker
+SWITCHING: 5-way pickup switch
+CONTROLS: 1 x master volume, 2 x master tone. Shotgun shell knobs.
+
+
+NOTES
+
+Comes with Original Hardshell Case, COA & Case candy'),
+(1,5000,'Gibson','Gibson True Historic ''60 Les Paul','Excellent','Electric guitars',2015,'US','BODY
+
+BODY TYPE: Solid body
+BODY MATERIAL: Mahogany
+BODY FINISH: Poly
+FINISH TYPE: Gloss 
+COLOR: Sunburst
+
+
+NECK
+
+NECK MATERIAL: Mahogany
+NECK SHAPE: “D”
+RADIUS: 12”
+1st NECK THICKNESS: .800"
+12st NECK THICKNESS: .930”
+FINGERBOARD MATERIAL: Rosewood
+FINGERBOARD INLAY: Trapezoid inlays
+NUMBER OF FRETS: 21
+SCALE LENGTH: 24.75”
+NUT WIDTH: 1.6875”
+
+
+HARDWARE
+
+BRIDGE/TAILPIECE: ABR-1 Bridge with Stopbar Tailpiece
+TUNERS: Kluson Single Line Single Ring Tuners
+
+
+ELECTRONICS
+
+NECK PICKUP: Gibson Custom Bucker
+BRIDGE PICKUP: Gibson Custom Bucker
+SWITCHING: 3-way pickup switch
+CONTROLS: 2 x master volume, 2 x master tone
+
+
+NOTES
+
+Comes with Original Hardshell Case, COA & Case candy'),
+(1,5000,'Jackson','Jackson Custom Shop SL2H Soloist Gravity Yellow w/ Blue Pickups','Mint','Electric guitars',2019,'US','BODY
+
+BODY TYPE: Solid body 
+BODY MATERIAL: Alder
+BODY FINISH: Gloss
+COLOR: Gravity Yellow w/ Blue Pickups
+
+
+NECK
+
+NECK MATERIAL: Maple
+NECK SHAPE: Jackson Speed Neck
+RADIUS: 12"-16" Compound Radius
+1st NECK THICKNESS: 
+12st NECK THICKNESS:
+FINGERBOARD MATERIAL: Ebony
+FINGERBOARD INLAY: Abalone Sharkfins
+NUMBER OF FRETS: 24, Jumbo
+SCALE LENGTH: 25.5"
+NUT WIDTH: 1.6875"
+
+
+HARDWARE
+
+BRIDGE/TAILPIECE: Floyd Rose® Original Double-Locking Tremolo
+TUNERS: Jackson® Sealed Die-Cast
+
+
+ELECTRONICS
+
+NECK PICKUP: Seymour Duncan® ''59™ SH-1N
+BRIDGE PICKUP: Seymour Duncan® JB™ TB-4
+SWITCHING: 3-way pickup switch
+CONTROLS: 1 x master volume, 1 x master tone
+
+
+NOTES
+
+Comes with Original Hardshell Case, Case candy.'),
+(1,5000,'Vigier','Vigier Excalibur Supra Rock Art','Mint','Electric guitars',2019,'FR','BODY
+
+BODY TYPE: Solid body
+BODY MATERIAL: Natural Aged Alder
+BODY FINISH: Lacquer
+FINISH TYPE: Gloss
+COLOR: Blue Swirl
+
+
+NECK
+
+NECK MATERIAL: Maple naturally dried for 3 years
+NECK SHAPE: “D”
+RADIUS: 11.81” Radius
+1st NECK THICKNESS: .760”
+12st NECK THICKNESS: .880”
+FINGERBOARD MATERIAL: Rosewood
+FINGERBOARD INLAY: White Dots
+NUMBER OF FRETS: 24 Medium Stainless Steel Frets + 0 Fret
+SCALE LENGTH: 25.6”
+NUT WIDTH: 1.650”
+
+
+HARDWARE
+
+BRIDGE/TAILPIECE: Vigier Tremolo Bridge
+TUNERS: Schaller Locking Tuners
+
+
+ELECTRONICS
+
+NECK PICKUP: DiMarzio PAF Pro Humbucker
+MIDDLE PICKUP: DiMarzio FS-1 Single-coil
+BRIDGE PICKUP: DiMarzio Tone Zone Humbucker
+SWITCHING: 5-way pickup switch
+CONTROLS: 1 x master volume, 1 x master tone
+
+
+NOTES
+
+Comes with Original Hardshell Case, COA & Case candy'),
+(1,3000,'Fender','Fender American Stratocaster Vintage 1977 Olympic White','Very Good','Electric guitars',1977,'US','BODY
+
+BODY TYPE: Solid body
+BODY MATERIAL: Ash
+BODY FINISH: Poly
+FINISH TYPE: Gloss
+COLOR: Olympic White
+NECK
+
+NECK MATERIAL: Maple
+NECK SHAPE: “C”
+RADIUS: 7.25” Radius
+1st NECK THICKNESS: 0.84”
+12st NECK THICKNESS: 0.94”
+FINGERBOARD MATERIAL: Rosewood
+FINGERBOARD INLAY: MOP Dots
+NUMBER OF FRETS: 22, Medium 
+SCALE LENGTH: 25.5”
+NUT WIDTH: 1.625”
+HARDWARE
+
+BRIDGE/TAILPIECE: Fender diecast tremolo bridge with diecast saddles
+TUNERS: Fender 70s split shaft tuners (Made by Schaller)
+ELECTRONICS
+
+NECK PICKUP: Fender 70s Stratocaster single-coil 
+MIDDLE PICKUP: Fender 70s Stratocaster single-coil 
+BRIDGE PICKUP: Fender 70s Stratocaster single-coil 
+SWITCHING: 3-way pickup switch (*)
+CONTROLS: 1 x master volume, 2 x master tone 
+NOTES
+
+Comes with Original Hardshell Case.
+The neck is made in 1972
+Fender introduced the 5-way switch in 1977, so it''s possible the guitar was made in early 1977 when they were still using 3-way switches. (*)
+Fender switched from white to black plastic (pickguard, pickups, knobs,..) around 1975, and during this transition guitars may be seen with a mix & match of white and black plastic parts. A 1977 Strat could have mixed parts, but any later should be all black.'),
+(1,2800,'Ibanez','Ibanez J Custom RRG7 CHG','Mint','Electric guitars',2010,'JP','BODY
+
+BODY TYPE: Solid body 
+BODY MATERIAL: African Mahogany w/ quilted maple top
+COLOR: Hesonite Garnet
+
+
+NECK
+
+NECK MATERIAL: 5-piece maple/ walnut w/ VelveTouch finish
+NECK SHAPE: Super Wizard
+RADIUS: 12-16’’ Compound
+1st NECK THICKNESS: .680''''
+12st NECK THICKNESS: .770''''
+FINGERBOARD MATERIAL: Rosewood
+FINGERBOARD INLAY: Tree of Life
+NUMBER OF FRETS: 24
+SCALE LENGTH: 25½"
+NUT WIDTH: 1.889"
+
+
+HARDWARE
+
+BRIDGE/TAILPIECE: Edge Zero tremolo w/ ZPS3 stabilizer
+TUNERS: Gotoh SG381
+
+
+ELECTRONICS
+
+NECK PICKUP: DiMarzio Air Norton 
+MIDDLE PICKUP: DiMarzio True Velvet
+BRIDGE PICKUP: DiMarzio Tone Zone
+SWITCHING: 3-way
+CONTROLS: 1 x master volume, 1 x master tones
+
+
+NOTES
+
+Comes with Original Hardcase, COA'),
+(1,2500,'Ibanez','Ibanez JS2410 Joe Satriani Signature Electric Guitar w/case, Muscle Car Orange','Brand New','Electric guitars',2021,'JP','BODY
+
+BODY TYPE: Solid body 
+BODY MATERIAL: Alder
+BODY FINISH: Gloss Polyurethane
+COLOR: Muscle Car Orange
+
+
+NECK
+
+NECK MATERIAL: Rosewood
+NECK SHAPE: JS 3pc Maple/Bubinga neck
+RADIUS: 9.84”
+1st NECK THICKNESS: 0.787"
+12st NECK THICKNESS: 0.877"
+FINGERBOARD MATERIAL: Rosewood
+FINGERBOARD INLAY: Mother of Pearl
+NUMBER OF FRETS: 24, Jumbo
+SCALE LENGTH: 25.5"
+NUT WIDTH: 1.654”
+
+
+HARDWARE
+
+BRIDGE/TAILPIECE:
+TUNERS:
+
+
+ELECTRONICS
+
+NECK PICKUP: DiMarzio Satch Track Humbucker
+MIDDLE PICKUP: None
+BRIDGE PICKUP: DiMarzio Mo'' Joe Humbucker
+SWITCHING: 5-way blade pickup switch
+CONTROLS: 1 x master volume, 1 x master tones
+
+
+NOTES
+
+Hi-pass filter on volume control (push/pull)
+Coil-tap switch on tone control (push/pull)'),
+(1,1700,'PRS','2004 PRS McCarty 10 Top with Solid Rosewood Neck - Vintage Yellow','Very Good','Electric guitars',2004,'US','BODY
+
+BODY TYPE: Solid body
+BODY MATERIAL: Mahogany
+TOP MATERIAL: Maple 10 top , carved
+BODY FINISH: Poly
+FINISH TYPE: Gloss 
+COLOR: Vintage Yellow top, Cherry back
+NECK
+
+NECK MATERIAL: Solid Indian Rosewood
+NECK SHAPE: Wide/fat
+RADIUS: 10”
+1st NECK THICKNESS: .870"
+12st NECK THICKNESS: .950”
+FINGERBOARD MATERIAL: Rosewood
+FINGERBOARD INLAY: Abalone Birds
+NUMBER OF FRETS: 22, Medium Jumbo
+SCALE LENGTH: 25”
+NUT WIDTH: 1.6875”
+HARDWARE
+
+BRIDGE/TAILPIECE: Stop Tail
+TUNERS: Gold Locking Tuners (*)
+ELECTRONICS
+
+NECK PICKUP: PRS McCarty Humbucker
+BRIDGE PICKUP: PRS McCarty Humbucker
+SWITCHING: 3-way pickup switch
+CONTROLS: 1 x master volume, 1 x master tone
+NOTES
+
+Comes with Hardshell Case
+Tuners is not original (*)'),
+(1,1700,'Vigier','Vigier Excalibur Original Black Sparkle','Very Good','Electric guitars',2021,'FR','Body: Two center-joined pieces of solid, naturally aged Alder
+Neck: Maple naturally dried for 3 years
+Finish: Gloss
+Finish Color: Black Sparkle
+Neck shape: D
+Width of neck at nut: 42mm / 1.65"
+Fingerboard: Maple
+Radius: 300mm / 11.81"
+Frets: Medium Stainless Steel
+Number of frets: 24 + zero fret
+Pickups: SH5/SSL6/TB5
+Bridge: Vigier tremolo
+Include Hardshell Hiscox Case.'),
+(1,1000,'ESP','ESP LTD EC-1000','Brand New','Electric guitars',2023,'KR','BODY        
+Body Type: Solid body
+Body Shape: Eclipse
+Body Material: Mahogany
+Body Finish: Gloss
+Color: Black
+
+NECK        
+Neck Material: Mahogany
+Neck Shape: Thin U
+Neck Joint: Set-through
+Radius: 13.7"
+Fingerboard Material: Macassar Ebony
+Fingerboard Inlay: Abalone Flags
+Number of Frets: 24, Extra Jumbo, Stainless Steel
+Scale Length: 24.75"
+Nut Width: 1.65"
+Nut Material: Molded
+
+HARDWARE        
+Bridge/Tailpiece: TonePros Tune-O-Matic Bridge with Stopbar Tailpiece
+Tuners: Locking tuners
+
+ELECTRONICS        
+Neck Pickup: EMG 60 Humbucker
+Bridge Pickup: EMG 81 Humbucker
+Controls: 2 x volume, 1 x tone
+Switching: 3-way toggle pickup switch'),
+(1,800,'PRS','PRS SE Silver Sky Electric Guitar, Stone Blue','Brand New','Electric guitars',2022,'ID','BODY
+
+BODY TYPE: Solid body
+BODY MATERIAL: Poplar
+BODY FINISH: Gloss
+COLOR: Stone Blue
+
+
+NECK
+
+NECK MATERIAL: Maple
+NECK SHAPE: 635JM
+RADIUS: 8.5”
+1st NECK THICKNESS: .850"
+12st NECK THICKNESS: .950"
+FINGERBOARD MATERIAL: Rosewood
+FINGERBOARD INLAY: Small Birds
+NUMBER OF FRETS: 22
+SCALE LENGTH: 25.5”
+NUT WIDTH: 1.625"
+
+
+HARDWARE
+
+BRIDGE/TAILPIECE: 2-Piece Steel Tremolo
+TUNERS: Vintage-style Non-locking
+
+
+ELECTRONICS
+
+NECK PICKUP: PRS 635JM "S" Single-coil
+MIDDLE PICKUP: PRS 635JM "S" Single-coil
+BRIDGE PICKUP: PRS 635JM "S" Single-coil
+SWITCHING: 5-way pickup switch
+CONTROLS: 1 x master volume, 2 x master tone
+
+
+NOTES
+
+Comes with PRS GigBag.'),
+(1,600,'PRS','PRS SE 245 Electric Guitar - Vintage Sunburst','Brand New','Electric guitars',2021,'ID','BODY
+
+BODY TYPE: Solid body
+BODY MATERIAL: Mahogany
+TOP MATERIAL: Flame Maple
+BODY FINISH: Gloss
+COLOR: Vintage Sunburst
+
+
+NECK
+
+NECK MATERIAL: Mahogany
+NECK SHAPE: Wide Fat
+RADIUS: 10"
+1st NECK THICKNESS: .900”
+12st NECK THICKNESS: 1.000”
+FINGERBOARD MATERIAL: Rosewood
+FINGERBOARD INLAY: Birds
+NUMBER OF FRETS: 22, Medium Jumbo
+SCALE LENGTH: 24.5”
+NUT WIDTH: 1.6875"
+
+
+HARDWARE
+
+BRIDGE/TAILPIECE: PRS Adjustable Stoptail
+TUNERS: PRS Designed Tuners
+
+
+ELECTRONICS
+
+NECK PICKUP: PRS 245 "S" Humbucker
+BRIDGE PICKUP: PRS 245 "S" Humbucker
+SWITCHING: 3-way pickup switch
+CONTROLS: 2 x master volume, 2 x master tone
+
+
+NOTES
+
+Comes with PRS Gigbag'),
+(1,600,'Epiphone','Les Paul Standard 50s Heritage Cherry Sunburst','Brand New','Electric guitars',2023,'CN','BODY
+Model: Epiphone Les Paul Standard 50s, Heritage Cherry Sunburst
+Shape: Les Paul Standard
+Material: Mahogany
+Top: Maple Cap
+Binding: Cream
+
+NECK & FINGERBOARD
+Neck: Mahogany
+Profile: ''59 Rounded Medium C
+Nut Width: 1.693" / 43mm
+Scale Length: 24.75" / 628.65mm
+Number Of Frets: 22
+Nut: Graph Tech®
+
+HARDWARE & ELECTRONICS
+Bridge: Locktone™ ABR Tune-O-Matic
+Tailpiece: Stopbar
+Tuners: Epiphone Vintage Deluxe 18:1 Ratio Machine Heads
+Plating: Nickel
+Neck Pickup: Probucker-2™
+Bridge Pickup: Probucker-3™
+Controls: 2- Volume, 2- Tone CTS Electronics 50s Style Wiring'),
+(1,600,'Epiphone','Epiphone 1959 Les Paul Standard Outfit Electric Guitar, Aged Dark Burst','Brand New','Electric guitars',2022,'CN','BODY
+Body Material: Mahogany
+Body Shape: Les Paul
+Colour: Aged Dark Burst
+
+NECK & FINGERBOARD
+Neck Material: Mahogany
+Neck Profile: ’59 Rounded “C”
+Fingerboard Material: Indian Laurel
+Fingerboard Radius: 12"
+Fingerboard Inlays: Trapezoid
+Number of Frets: 22
+Fret Size: Medium Jumbo
+Nut: Graph Tech NuBone
+Nut Width: 1.693"
+Scale Length: 24.764"
+
+HARDWARE & ELECTRONICS
+Hardware Finish: Nickel
+Tuning Machines: Epiphone Deluxe with Vintage Tulip Keys
+Bridge: Epiphone LockTone ABR-1
+Pickguard: Cream
+Control Knobs: Gold Top Hats with Dial Pointers
+Pickup Configuration: HH
+Bridge Pickup: Gibson BurstBucker 3
+Neck Pickup: Gibson BurstBucker 2
+Controls: 2 Volume, 2-Tone, CTS Pots, Mallory Capacitors, 50s Era Wiring
+Pickup Switching: 3-Way Switchcraft Toggle'),
+(1,500,'ESP','ESP LTD EC-256','Mint','Electric guitars',2020,'VN','BODY        
+Body Type: Solid body
+Body Shape: Eclipse
+Body Material: Mahogany
+Body Finish: Satin
+Color: Black
+
+NECK        
+Neck Material: 3-piece Mahogany
+Neck Shape: Thin U
+Neck Joint: Set Neck
+Radius: 13.7"
+Fingerboard Material: Roasted Jatoba
+Fingerboard Inlay: Pearloid Flags
+Number of Frets: 22, Extra Jumbo
+Scale Length: 24.75"
+Nut Width: 1.653"
+Nut Material: Molded
+
+HARDWARE        
+Bridge/Tailpiece: Tune-O-Matic Bridge with Stopbar Tailpiece
+Tuners: LTD
+
+ELECTRONICS        
+Neck Pickup: ESP LH-150N Humbucker
+Bridge Pickup: ESP LH-150B Humbucker
+Controls: 2 x volume, 1 x tone (push/pull coil-tap)
+Switching: 3-way toggle pickup switch'),
+(1,400,'Ibanez','Ibanez AZES40-MGR Electric Guitar, Mint Green','Brand New','Electric guitars',2023,'ID','BODY
+
+BODY TYPE: Solid body 
+BODY MATERIAL: Poplar
+BODY FINISH: Gloss 
+COLOR: Mint Green
+
+
+
+NECK
+
+NECK MATERIAL: Maple
+NECK SHAPE: AZES C
+RADIUS: 10”
+1st NECK THICKNESS: .810"
+12st NECK THICKNESS: .910"
+FINGERBOARD MATERIAL: Jatoba
+FINGERBOARD INLAY: Dots
+NUMBER OF FRETS: 22, Medium Nickel
+SCALE LENGTH: 25"
+NUT WIDTH: 1.653"
+NUT MATERIAL: Plastic
+
+
+
+HARDWARE
+
+BRIDGE/TAILPIECE: F106 Tremolo Bridge with Round Steel Saddles
+TUNERS: Ibanez Split-shaft
+
+
+
+ELECTRONICS
+
+NECK PICKUP: Ibanez Essential Single-coil
+MIDDLE PICKUP: Ibanez Essential Single-coil
+BRIDGE PICKUP: Ibanez Essential Accord Humbucker
+SWITCHING: 1 x Alter Switch, 5-way blade pickup switch
+CONTROLS: 1 x master volume, 1 x master tone');
